c248a8190232218bdf2b04580adf2e87bdf21470
Joshua Madden <Joshua Madden>
2016-01-12 08:21:32 -0700

    findKeyword method

---
+++ answers.md
@@ -0,0 +1,4 @@
+Activity 3: Better Keyword Detection
+	Exploration:
+		add int notFoundPsn = sample.indexOf("slow"); - response, sample.indexOf("quick") = 4, sample.toLowerCase() = the quick brown fox jumped over the lazy dog., After toLowerCase(), sample = The quick brown fox jumped over the lazy dog.
+		
--- src/StringExplorer.java
+++ src/StringExplorer.java
@@ -8,7 +8,7 @@ public class StringExplorer {
 
 	public static void main(String[] args) {
 		String sample = "The quick brown fox jumped over the lazy dog.";
-
+		
 		// Demonstrate the indexOf method.
 		int position = sample.indexOf("quick");
 		System.out.println("sample.indexOf(\"quick\") = " + position);
@@ -19,7 +19,8 @@ public class StringExplorer {
 		System.out.println("After toLowerCase(), sample = " + sample);
 
 		// Try other methods here:
-
+		int notFoundPsn = sample.indexOf("slow");
+		System.out.println("sample.indexOf(\"slow\") = " + notFoundPsn);
 	}
 
 }
