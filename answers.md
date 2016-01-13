6e7b26b5d0f0029b9b737256243028c2a30db10a
Joshua Madden <Joshua Madden>
2016-01-12 19:48:13 -0700

    finish activity4

---
+++ answers.md
@@ -0,0 +1,3 @@
+# Activity 4
+
+This pattern works well most of the time although it does not work when a pronoun shows up in the *something*. The pronoun is not converted, *my --> your*  **or**  *your --> my*, therefore making little sense. To fix that, one would need to first detect the statement and then detect the need to change the pronouns.
--- src/Magpie4.java
+++ src/Magpie4.java
@@ -46,6 +46,14 @@ public class Magpie4 {
 		// Responses which require transformations
 		else if (findKeyword(statement, "I want to", 0) >= 0) {
 			response = transformIWantToStatement(statement);
+		} else if (findKeyword(statement, "I want", 0) >= 0) {
+			response = transformIWantStatement(statement);
+		} else if (findKeyword(statement, "I", 0) == 0) {
+			if (findKeyword(statement, "you", 0) > 0) {
+				response = transformIYouStatement(statement);
+			} else {
+				response = getRandomResponse();
+			}
 		}
 
 		else {
@@ -81,7 +89,32 @@ public class Magpie4 {
 		String restOfStatement = statement.substring(psn + 9).trim();
 		return "What would it mean to " + restOfStatement + "?";
 	}
+	
+	private String transformIWantStatement(String statement) {
+		statement = statement.trim();
+		String lastChar = statement.substring(statement.length() - 1);
+		if (lastChar.equals(".")) {
+			statement = statement.substring(0, statement.length() - 1);
+		}
+		int psn = findKeyword(statement, "I want", 0);
+		String restOfStatement = statement.substring(psn + 6).trim();
+		return "Would you really be happy if you had " + restOfStatement + "?"; 
+	}
 
+	private String transformIYouStatement(String statement) {
+		statement = statement.trim();
+		String lastChar = statement.substring(statement.length() - 1);
+		if (lastChar.equals(".")) {
+			statement = statement.substring(0, statement.length() - 1);
+		}
+
+		int psnOfI = findKeyword(statement, "I", 0);
+		int psnOfYou = findKeyword(statement, "you", psnOfI + 2);
+
+		String restOfStatement = statement.substring(psnOfI + 2, psnOfYou)
+				.trim();
+		return "Why do you " + restOfStatement + " me?";
+	}
 	/**
 	 * Take a statement with "you <something> me" and transform it into
 	 * "What makes you think that I <something> you?"
